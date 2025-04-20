b92f6d6d72427b685a38318f35c7121f36842012
UFOBob069 <UFOBob069@yahoo.com>
2025-04-20 13:38:40 -0500

    Fix page props typing by using Next.js built-in types

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,12 +1,5 @@
 import JobPostContent from './JobPostContent';
 
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-async function JobPostPage({ params, searchParams }: Props) {
+export default async function JobPostPage({ params }: { params: { id: string } }) {
   return <JobPostContent postId={params.id} />;
-}
-
-export default JobPostPage; 
+} 

77ae82c61ca180c4a634fc7e7d92c81872f00546
UFOBob069 <UFOBob069@yahoo.com>
2025-04-20 14:30:10 -0500

    Use Next.js Metadata type and make page component synchronous

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,15 +1,16 @@
+import { Metadata } from 'next';
 import JobPostContent from './JobPostContent';
 
 type Props = {
   params: { id: string };
 };
 
-export default async function Page({ params }: Props) {
-  return <JobPostContent postId={params.id} />;
-}
-
-export async function generateMetadata({ params }: Props) {
+export function generateMetadata({ params }: Props): Metadata {
   return {
     title: `Post ${params.id}`,
   };
+}
+
+export default function Page({ params }: Props) {
+  return <JobPostContent postId={params.id} />;
 } 

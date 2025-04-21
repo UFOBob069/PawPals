5dad503b6eb105856be915f8609eec9849237db9
UFOBob069 <UFOBob069@yahoo.com>
2025-04-20 19:06:59 -0500

    Remove metadata export from page component

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,9 +1,4 @@
-import { Metadata } from 'next';
 import JobPostContent from './JobPostContent';
-
-export const metadata: Metadata = {
-  title: 'Job Post',
-};
 
 export default function Page({ params }: { params: { id: string } }) {
   return <JobPostContent postId={params.id} />;
